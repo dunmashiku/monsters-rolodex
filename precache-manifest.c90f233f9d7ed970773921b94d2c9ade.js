fdcced4ecf116bec3aaeef7c5383340ba2eb1652
Dun Mashiku <dunmashiku@gmail.com>
2020-04-01 23:16:30 -0400

    Updates

---
+++ precache-manifest.c90f233f9d7ed970773921b94d2c9ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1ac8dd0f4b3d14c539837d7c3f1197",
+    "url": "/dunmashiku/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "430caf3cd44e771662ff",
+    "url": "/dunmashiku/monsters-rolodex/static/css/main.c14db730.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/dunmashiku/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dunmashiku/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430caf3cd44e771662ff",
+    "url": "/dunmashiku/monsters-rolodex/static/js/main.e18fea92.chunk.js"
+  },
+  {
+    "revision": "93fcc7c6beae2e77c1c1",
+    "url": "/dunmashiku/monsters-rolodex/static/js/runtime-main.d3ab68bb.js"
+  }
+]);
